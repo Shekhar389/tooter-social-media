96f9769fdc73f0a8a43c627e13e1c3eb7656749d
Shekhar Kumar <kshekhar2807@gmail.com>
2023-07-18 15:56:53 +0530

    Defined Two Api end Point and Allowed CORS Policy

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(bodyparser.json())
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Origin','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Origin','Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
     next();
 })
 app.use('/feed',feedRoutes);
--- controller/feed.js
+++ controller/feed.js
@@ -1,10 +1,19 @@
 exports.getPosts=(req,res,next)=>{
-    res.status(200).json({posts: [{title : 'First Post',content :'This is first post'}]})
+    res.status(200).json({posts: [{_id:'1' ,title : 'Siddhant Singh',content :'This is first post',imageUrl:'images/1109789.png',creator:{name:'Shekhar'},createdAt:new Date()}]})
 }
 exports.createPost=(req,res,next)=>{
     const title=req.body.title;
     const content=req.body.content;
+    
     res.status(201).json({message:'Post created successfully',
-            post : { id: new Date().toISOString() ,title:title ,content:content}
+            post : { 
+                id: new Date().toISOString() ,
+                title:title ,
+                content:content,
+                creator:{
+                    name : 'Shekhar',
+                    createdAt:new Date(),
+                        }
+        }
     })
 }
