deb7b78e0b3a27900f5650093ac9e15a8559ba13
Shekhar Kumar <kshekhar2807@gmail.com>
2023-07-18 17:29:44 +0530

    Validation,database,ErrorHandling

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express=require('express')
-const feedRoutes=require('./routes/feed')
-const bodyparser=require('body-parser')
+const path=require('path')
+const express = require('express');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+const feedRoutes = require('./routes/feed');
+
 const app = express();
 //app.use(bodyparser.urlencoded({ extended: true }))//used for form
-app.use(bodyparser.json())
+app.use(bodyparser.json());
+app.use('/images',express.static(path.join(__dirname,'images')));//Images folder as static folder
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
@@ -11,4 +16,17 @@ app.use((req,res,next)=>{
     next();
 })
 app.use('/feed',feedRoutes);
-app.listen(8080)
+app.use((error,req,res,next)=>{
+    console.log(error);
+    const message=error.message;
+    const status=error.statusCode;
+    res.status(status).json({message:message});
+})
+mongoose
+  .connect(
+    'mongodb+srv://kshekhar2807:mKMIOJ2RI6Q6gawO@cluster0.gcxkevb.mongodb.net/tooter?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
--- controller/feed.js
+++ controller/feed.js
@@ -1,19 +1,48 @@
-exports.getPosts=(req,res,next)=>{
-    res.status(200).json({posts: [{_id:'1' ,title : 'Siddhant Singh',content :'This is first post',imageUrl:'images/1109789.png',creator:{name:'Shekhar'},createdAt:new Date()}]})
-}
-exports.createPost=(req,res,next)=>{
-    const title=req.body.title;
-    const content=req.body.content;
-    
-    res.status(201).json({message:'Post created successfully',
-            post : { 
-                id: new Date().toISOString() ,
-                title:title ,
-                content:content,
-                creator:{
-                    name : 'Shekhar',
-                    createdAt:new Date(),
-                        }
+const { validationResult } = require('express-validator');
+const Post = require('../models/post');
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+      posts: [
+        {
+          _id: '1',
+          title: 'First Post',
+          content: 'This is the first post!',
+          imageUrl: 'images/duck.jpg',
+          creator: {
+            name: 'Maximilian'
+          },
+          createdAt: new Date()
         }
-    })
-}
+      ]
+    });
+  };
+  exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error=new Error("Validation failed, entered data is incorrect.");
+        error.statusCode=422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    const post = new Post({
+      title: title,
+      content: content,
+      imageUrl: 'images/ducks.png',
+      creator: { name: 'Maximilian' }
+    });
+    post
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: 'Post created successfully!',
+          post: result
+        });
+      })
+      .catch(err => {
+        if(!err.statusCode){
+            err.statusCode=500;
+        }
+        next(err);
+      });
+  };
